9c867d674a69b126dd3b040e706bbabe8be06a52
Janet Matsen <janetmatsen@gmail.com>
2017-03-10 23:54:11 +0000

    use abs path for rnaseq fastq counting path regex

--- rnaseq/scripts/count_fastq_reads.sh
+++ rnaseq/scripts/count_fastq_reads.sh
@@ -1,4 +1,4 @@
-FASTQS='data/*/Raw_Data/*.fastq.gz'
+FASTQS='/work/rnaseq/data/*/Raw_Data/*.fastq.gz'
 RESULTS=rna_fastq_read_counts.tsv
 COUNT_SCRIPT=/work/m4b_binning/assembly/data/sample_info/count_reads_in_each_sample.sh
 
