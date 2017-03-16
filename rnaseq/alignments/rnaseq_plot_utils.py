0a95c6b4921dac2e69ea2818255a5cdee6f59172
Janet Matsen <janetmatsen@gmail.com>
2017-03-16 20:25:05 +0000

    tidied RNA-seq gene expression bar plots: easter colors

--- rnaseq/alignments/rnaseq_plot_utils.py
+++ rnaseq/alignments/rnaseq_plot_utils.py
@@ -6,6 +6,8 @@ import seaborn as sns
 from matplotlib import rc
 rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
 
+from collections import OrderedDict # for plot order
+
 from plot_facets import bar_facets_from_pivoted_df
 from plot_facets import add_vline_to_all_subplots
 
@@ -15,24 +17,26 @@ def plot_underscore_bars(input_df, filename=None, portrait=True):
     """
     # make a copy, for safety.
     dataframe = input_df.copy()
-    order_list = ['sum(frac RNA-seq mapped to genes)',
-                  'frac of RNA reads: __alignment_not_unique',
-                  'frac of RNA reads: __ambiguous', 'frac of RNA reads: __no_feature',
-                  'frac of RNA reads: __not_aligned', 'frac of RNA reads: __too_low_aQual']
 
-    # specify colors
-    colors = ['#31a354', # dark green,
-              'w',
-              'w',
-              '#bdbdbd', # light gray
-              #'#f03b20', # dark orange
-              '#feb24c', # light orange
-              '#fa9fb5', # pink
-             ]
+    # prepare titles.
+    # 'frac of RNA reads: __no_feature' --> 'no feature'
+    dataframe.columns = [c.replace('frac of RNA reads: __', '') for c in dataframe.columns]
+    dataframe.columns = [c.replace('_', ' ') for c in dataframe.columns]
+    dataframe.rename(columns={'sum(frac RNA-seq mapped to genes)':'mapped to genes'}, inplace=True)
+
+    colord = OrderedDict([
+        ('mapped to genes','#31a354'), # dark green
+        ('no feature', '#bdbdbd'), # light gray
+        ('too low aQual', '#fa9fb5'), # pink
+        ('not aligned', '#feb24c'), # light orange
+        ('alignment not unique','w'),
+        ('ambiguous','w'),
+        ])
 
     fig = bar_facets_from_pivoted_df(
         input_df=dataframe, pre_pivoted=True, x='week',
-        order_list=order_list, color_list=colors,
+        order_list=list(colord.keys()),
+        color_list=list(colord.values()),
         portrait=portrait, filename=filename)
     fig = add_vline_to_all_subplots(fig, x=10.5-4, ymin=0, ymax=1, color='#636363')
 
