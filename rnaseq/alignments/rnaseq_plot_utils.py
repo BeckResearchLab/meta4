641ada55cfa695c019802095f99a1b4cca30274b
Janet Matsen <janetmatsen@gmail.com>
2017-03-16 19:40:18 +0000

    reasonable plot for rna-seq mapping.  Easter colors, says Dave

--- rnaseq/alignments/rnaseq_plot_utils.py
+++ rnaseq/alignments/rnaseq_plot_utils.py
@@ -7,6 +7,7 @@ from matplotlib import rc
 rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
 
 from plot_facets import bar_facets_from_pivoted_df
+from plot_facets import add_vline_to_all_subplots
 
 
 def plot_underscore_bars(input_df, filename=None, portrait=True):
@@ -14,18 +15,24 @@ def plot_underscore_bars(input_df, filename=None, portrait=True):
     """
     # make a copy, for safety.
     dataframe = input_df.copy()
-    order_list = ['frac of RNA reads: __alignment_not_unique',
+    order_list = ['sum(frac RNA-seq mapped to genes)',
+                  'frac of RNA reads: __alignment_not_unique',
                   'frac of RNA reads: __ambiguous', 'frac of RNA reads: __no_feature',
                   'frac of RNA reads: __not_aligned', 'frac of RNA reads: __too_low_aQual']
 
-    # specify colors, dark to light.
-    colors = ['#810f7c', '#8856a7', '#8c96c6', '#9ebcda',    # purples
-              '#006d2c', '#74c476'] # greens
+    # specify colors
+    colors = ['#31a354', # dark green,
+              'w',
+              'w',
+              '#bdbdbd', # light gray
+              #'#f03b20', # dark orange
+              '#feb24c', # light orange
+              '#fa9fb5', # pink
+             ]
 
-    fig = plot_facets.bar_facets_from_pivoted_df(
-        input_df=dataframe, pivoted=True,
-        x='Genus italics', y='fraction of reads',
-        order_list=order_list_italic, color_list=colors,
+    fig = bar_facets_from_pivoted_df(
+        input_df=dataframe, pre_pivoted=True, x='week',
+        order_list=order_list, color_list=colors,
         portrait=portrait, filename=filename)
     fig = add_vline_to_all_subplots(fig, x=10.5-4, ymin=0, ymax=1, color='#636363')
 
