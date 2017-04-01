c320958170db06d43af2009d2c3312bf7754b9e4
Janet Matsen <janetmatsen@gmail.com>
2017-04-01 13:51:10 +0000

    make gff tsv have int type gene lengths.  Also removed whitespace from end of lines

--- m4b_binning/assembly/prokka/contigs/glue_together_gffs.py
+++ m4b_binning/assembly/prokka/contigs/glue_together_gffs.py
@@ -10,7 +10,7 @@ import pandas as pd
 def prep_filename(gff_dir):
     # strip off trailing '/'
     gff_dir = gff_dir.rstrip('/')
-    return os.path.join(gff_dir, '{}_gffs_concatenated.gff'.format(gff_dir))    
+    return os.path.join(gff_dir, '{}_gffs_concatenated.gff'.format(gff_dir))
 
 def get_group_name(filepath):
     m = re.search(r'_group_([0-9]+)', filepath)
@@ -26,10 +26,10 @@ def combine_gffs(gff_file_list, filename):
         file_num = 1
         for f in gff_file_list:
             print('get the good stuff from {}'.format(f))
-            group = get_group_name(f) 
+            group = get_group_name(f)
             with open(f) as f1:
                 for line_num, line in enumerate(f1):        #keep the header from file1
-                    # The first line is `##gff-version 3`.  Keep that for the first file. 
+                    # The first line is `##gff-version 3`.  Keep that for the first file.
                     if (file_num == 1) and (line_num == 0):
                         outfile.write(line)
                     # The end of the file has the entire FASTA sequence glued on.  Remove it.
@@ -37,16 +37,17 @@ def combine_gffs(gff_file_list, filename):
                         break
                     # Delete subsequent lines like `##sequence-region k141_461591 1 2140`
                     elif (line_num > 0) and line.startswith('##'):
+                        print('skip line: {}'.format(line))
                         continue
                     else:
-                        # Need to give each file unique ID's. If not, each file has an ID like  
+                        # Need to give each file unique ID's. If not, each file has an ID like
                         # ID=contigs_longer_than_1500bp_00001
                         # and duplicates are a problem.
                         m = re.search('(contigs_[_a-z0-9]+bp)', line)
                         if m:
                             general_name = m.group(1)
                             new_name = general_name + '_group_' + group
-                            line_edited = re.sub(general_name, new_name, line) 
+                            line_edited = re.sub(general_name, new_name, line)
                             outfile.write(line_edited)
                         else:
                             print("failed to find re.search for '(contigs_[_a-z0-9]+bp)' in '{}'".format(line))
@@ -61,23 +62,27 @@ def make_gene_table(gff_filename):
     # Don't be as careful getting the gene products because we can go to the end of the line
     raw['product'] = raw['description'].str.extract('product=(.*)', expand=True)
     raw['bp'] = raw['stop'] - raw['start'] + 1
+    # remove rows with contig == '##gff-version 3'
+    raw = raw[ ~ raw['contig'].str.contains('##gff-version')]
+    print(raw[raw['bp'].isnull()])
+    raw['bp'] = raw['bp'].astype(int)
     raw[['ID', 'product', 'bp']].to_csv(gff_filename + '.genes.tsv', sep='\t', index=False)
-    
-    
+
+
 
 if __name__ == '__main__':
     # expect python 3 to be used.  Source activate py3
-    assert sys.version_info > (3, 0), 'expected python 3; got {}'.format(sys.version)    
+    assert sys.version_info > (3, 0), 'expected python 3; got {}'.format(sys.version)
 
     parser = argparse.ArgumentParser()
     parser.add_argument('gff_parent_folder', help='parent folder to get gffs from')
     args = parser.parse_args()
-    
+
     if args.gff_parent_folder is None:
         print(args.print_help())
     else:
         print('gff parent folder: {}'.format(args.gff_parent_folder))
-    
+
     gff_file_list = []
     search_path = args.gff_parent_folder + '/**/*.gff'
     print('look for all gff files at paths like {}'.format(search_path))
@@ -87,7 +92,7 @@ if __name__ == '__main__':
 
     filename = prep_filename(args.gff_parent_folder)
     print('save concatenated gffs to {}'.format(filename))
-    
+
     # combine the found gff files
     combine_gffs(gff_file_list, filename)
     make_gene_table(filename)
