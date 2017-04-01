17ce9bef93a2e36e7d275e10594f32004bf94df0
Janet Matsen <janetmatsen@gmail.com>
2017-04-01 00:39:56 +0000

    add gene names to networkx graphs + ipynb exploration

--- rnaseq/pcor_new/networkx/networkx_helpers.py
+++ rnaseq/pcor_new/networkx/networkx_helpers.py
@@ -28,12 +28,34 @@ def load_df(fname):
     print('number of edges (dataframe rows): {}'.format(dataframe.shape[0]))
     return dataframe
 
-def load_network_from_df(network, df):
+def get_unique_nodes(df):
+    df_nodes = df[['gene A', 'product A']].drop_duplicates()
+    df_nodes.rename(columns={'gene A':'gene', 'product A':'product'}, inplace=True)
+    df_nodes = \
+        df_nodes.merge(df[['gene B', 'product B']].drop_duplicates().rename(
+            columns={'gene B':'gene', 'product B':'product'}))
+    return df_nodes
+
+def add_nodes_from_df(network, df):
+    # get set of unique nodes.
+    startTime = datetime.now()
+    df_nodes = get_unique_nodes(df)
+    # load nodes
+    print(df_nodes.head())
+    for idx, row in df_nodes.iterrows():
+        network.add_node(n=row['gene'], attr_dict={'product':row['product']})
+    print('networkx node adding time: {}'.format(datetime.now() - startTime))
+    return network
+
+
+def add_edges_from_df(network, df):
+    startTime = datetime.now()
     for idx, row in df.iterrows():
     # add_edge: The nodes u and v will be automatically added if
     # they are not already in the graph.
         network.add_edge(row['gene A'], row['gene B'],
                     attr_dict = {'pcor': row['pcor']} )
+    print('networkx edge adding time: {}'.format(datetime.now() - startTime))
     return network
 
 def load_network(fname):
@@ -41,11 +63,10 @@ def load_network(fname):
     num_genes = len(set(df['gene A'].drop_duplicates().tolist() + df['gene B'].drop_duplicates().tolist()))
     print('number of unique nodes: {}'.format(num_genes))
 
-    startTime = datetime.now()
     #n = networkx.DiGraph()  # directed
-    n = networkx.DiGraph()   # undirected
-    network = load_network_from_df(n, df)
-    print('networkx DiGraph filling time: {}'.format(datetime.now() - startTime))
-    return network
+    n = networkx.Graph()   # undirected
 
+    n = add_nodes_from_df(network=n, df=df)
+    n = add_edges_from_df(network=n, df=df)
 
+    return n
