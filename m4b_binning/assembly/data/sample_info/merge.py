a4348d266791e3b4ce95559cae8641020e44a5be
Janet Matsen <janetmatsen@gmail.com>
2017-04-03 14:22:23 +0000

    method to make a legend-friendly translation of cryptic sample id to something like low_

--- m4b_binning/assembly/data/sample_info/merge.py
+++ m4b_binning/assembly/data/sample_info/merge.py
@@ -1,7 +1,26 @@
 import pandas as pd
 
-si = pd.read_csv('sample_info.tsv', sep='\t')
-cr = pd.read_csv('meta4_sample_names--cryptic_to_sample_number.tsv', sep='\t')
+def make_merged_df():
+    si = pd.read_csv('sample_info.tsv', sep='\t')
+    cr = pd.read_csv('meta4_sample_names--cryptic_to_sample_number.tsv', sep='\t')
 
-m = pd.merge(cr, si, how='outer')
-m.to_csv('sample_info_w_cryptic.tsv', sep='\t', index=False)
+    m = pd.merge(cr, si, how='outer')
+    return m
+
+def prep_sample_desc_string(merged_df, tex=False):
+    if tex:
+        O2 = '_$O_2$_'
+    else:
+        O2 = '_O2_'
+    merged_df['descriptive string'] = merged_df['oxygen'] + O2 + 'week_' +  merged_df['week'].astype(str) + '_rep_' +  merged_df['replicate'].astype(str)
+    return merged_df
+
+
+
+if __name__ == '__main__':
+    m = make_merged_df()
+    m = prep_sample_desc_string(m, tex=True)
+    print(m.head(3))
+    m.to_csv('sample_info_w_cryptic.tsv', sep='\t', index=False)
+
+
