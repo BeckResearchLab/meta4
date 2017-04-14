e507e3c043c672b71875d961a1da8e2572f8f606
Janet Matsen <janetmatsen@gmail.com>
2017-04-14 23:53:33 +0000

    kinda out of date now, but lowered percent on this GeneNet running script

--- rnaseq/pcor_new/GeneNet/RUN.sh
+++ rnaseq/pcor_new/GeneNet/RUN.sh
@@ -10,7 +10,8 @@ input_fname=input_to_R--no_features_dropped.tsv
 echo filename for all features as percent of fastq: $input_fname
 python prep_input.py --dest_filename $input_fname > $script.log 2>&1
 
-percent=0.001
+#percent=0.001 # top million edges --> 19686 nodes, but almost no pMMO
+percent=0.05
 Rscript $script -f $input_fname -p $percent >> $script.log 2>&1
 # 170410 took half of the machine memory.  117681/245859MB --> 117GB
 # This 30k input gene run seems noticeably slower than for the 28k version prior to the trimming fix
